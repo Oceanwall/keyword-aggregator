b53b10d64767662283406156be1dbff9fe987365
Oceanwall <matthew853211@gmail.com>
2018-06-13 21:00:34 -0700

    Beginning to integrate Google jobsearch functionality

---
+++ searchScripts/googleJobSearch.js
@@ -0,0 +1,14 @@
+//for more info on how google auto authentication works with env variables, go here
+//https://cloud.google.com/docs/authentication/getting-started
+
+//Search basics can be found here
+//https://cloud.google.com/job-discovery/docs/search
+
+
+function getJobs() {
+  console.log("hello world");
+}
+
+module.exports = {
+  googleJobSearch: getJobs,
+}
